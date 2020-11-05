ff3a5717f02be5b4f30e4421adf5acf494dd1fc1
backo2020 <jhon.gonzalez.garcia.2011@gmail.com>
2020-11-05 15:05:45 -0500

    hola

---
+++ laura.js
@@ -0,0 +1,19 @@
+let resultado;
+console.log(resultado);
+
+function result(date){
+resultado = date
+console.log(resultado);
+if(resultado == "no"){
+    let r1 = confirm("estas segura de tu respuesta ?")
+    if(r1 == true){
+        let r2 = confirm("completamente segura ?")
+            if(r2 == true){
+                let r3 = confirm("piensalo bien, tal vez yo soy el hombre adecuado para ti, esta segura de decir que no ?")
+            }
+    }
+}else{
+    alert("Gracias hermosa !! te prometo que dare todo para hacerte feliz a mi lado ")
+}
+}
+
